19e47e2ff1a5159122ec92f605d8665f254deb22
zabealbe <zabeoalberto99@gmail.com>
2021-08-11 19:03:20 +0200

    fix: automatic loop device
    fixed hardcoded "loop10" device with an automatic search for the first free loop device with losetup --find

--- build-arch-arm-img.sh
+++ build-arch-arm-img.sh
@@ -13,8 +13,6 @@ TARGET_ZIP_MD5=${TARGET_ZIP}.md5
 MD5_URL=${IMG_URL}.md5
 MD5_NAME=${MD5_URL##*/}
 
-losetup /dev/loop10 && exit 1 || true
-
 ## Check cache and maybe download
 mkdir -p $IMG_DIR
 pushd $IMG_DIR
@@ -31,24 +29,24 @@ popd
 
 # Set up image file
 truncate -s 1500M "${TARGET_IMAGE}"
-losetup /dev/loop10 "${TARGET_IMAGE}"
-parted -s /dev/loop10 mklabel msdos
-parted -s /dev/loop10 mkpart primary fat32 -a optimal -- 0% 100MB
-parted -s /dev/loop10 set 1 boot on
-parted -s /dev/loop10 unit mb mkpart primary ext2 -a optimal -- 100MB 100%
-parted -s /dev/loop10 print
-mkfs.vfat -I -n SYSTEM /dev/loop10p1
-mkfs.ext4 -F -L root -b 4096 -E stride=4,stripe_width=1024 /dev/loop10p2
+LOOP_DEVICE=$(losetup --show --find "${TARGET_IMAGE}") # util-linux v2.21 or higher
+parted -s "${LOOP_DEVICE}" mklabel msdos
+parted -s "${LOOP_DEVICE}" mkpart primary fat32 -a optimal -- 0% 100MB
+parted -s "${LOOP_DEVICE}" set 1 boot on
+parted -s "${LOOP_DEVICE}" unit mb mkpart primary ext2 -a optimal -- 100MB 100%
+parted -s "${LOOP_DEVICE}" print
+mkfs.vfat -I -n SYSTEM "${LOOP_DEVICE}"p1
+mkfs.ext4 -F -L root -b 4096 -E stride=4,stripe_width=1024 "${LOOP_DEVICE}"p2
 
 # Mount image
 mkdir -p root
-mount /dev/loop10p2 root
+mount "${LOOP_DEVICE}"p2 root
 
 # Copy image contents over
 bsdtar xfz "${IMG_PATH}" -C root
 mv root/boot root/boot-temp
 mkdir -p root/boot
-mount /dev/loop10p1 root/boot
+mount "${LOOP_DEVICE}"p1 root/boot
 mv root/boot-temp/* root/boot/
 rm -rf root/boot-temp
 
@@ -57,7 +55,7 @@ sed -i "s/ defaults / defaults,noatime /" root/etc/fstab
 
 # Cleanup
 umount root/boot root
-losetup -d /dev/loop10
+losetup -d "${LOOP_DEVICE}"
 rm -rf root
 
 # Zip img
